54c547a707cfb96cc5e5e08b848de93eccf1709c
Max Baumann <info@fosefx.com>
2021-03-01 23:19:14 +0100

    other algo

--- source/bttv/ChatMessageDeligateWrapper.java
+++ source/bttv/ChatMessageDeligateWrapper.java
@@ -5,6 +5,7 @@ import tv.twitch.chat.ChatMessageInfo;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
 import tv.twitch.android.models.chat.MessageToken;
 import tv.twitch.android.models.chat.MessageToken.TextToken;
 import tv.twitch.android.models.chat.MessageToken.EmoticonToken;
@@ -19,13 +20,14 @@ public class ChatMessageDeligateWrapper extends ChatMessageDelegate {
     @Override
     public List<MessageToken> getTokens() {
         List<MessageToken> orig = super.getTokens();
+        int channel = Data.currentBroadcasterId;
 
         // Don't add Emotes, when we dont have the chnnel's emotes (yet)
-        if (!Data.channelHasEmotes(Data.currentBroadcasterId)) {
+        if (!Data.channelHasEmotes(channel)) {
             return orig;
         }
 
-        ArrayList<MessageToken> newTokens = new ArrayList<>();
+        ArrayList<MessageToken> newTokens = new ArrayList<>(orig.size() + 5);
 
         for (MessageToken token : orig) {
             // possible issue: emotes won't work in e.g. MentionToken or BitsToken
@@ -37,33 +39,25 @@ public class ChatMessageDeligateWrapper extends ChatMessageDelegate {
             TextToken text = (TextToken) token;
             String[] words = text.getText().split(" ");
 
-            int mergeStart = 0;
+            String currentText = "";
             for (int i = 0; i < words.length; i++) {
                 String word = words[i];
-                Emote emote = Data.getEmote(word, Data.currentBroadcasterId);
-                if (emote != null) {
-
-                    String merge = mergePrevious(words, mergeStart, i);
-                    newTokens.add(new TextToken(merge, text.getFlags()));
-                    mergeStart = i + 1;
-
-                    newTokens.add(new EmoticonToken(word, "BTTV-" + emote.id));
+                Emote emote = Data.getEmote(word, channel);
+                if (emote == null) {
+                    currentText += word + " ";
+                    continue;
                 }
+                newTokens.add(new TextToken(currentText, text.getFlags()));
+                newTokens.add(new EmoticonToken(word, "BTTV-" + emote.id));
+                currentText = "";
             }
 
-            String merge = mergePrevious(words, mergeStart, words.length);
-            newTokens.add(new TextToken(merge, text.getFlags()));
+            if (currentText.length() > 0) {
+                newTokens.add(new TextToken(currentText, text.getFlags()));
+            }
         }
 
         return newTokens;
-    }
-
-    private static String mergePrevious(String[] words, int start, int end) {
-        String merge = start != 0 ? " " : ""; // space when we added an emote before it
-        for (int j = start; j < end; j++) {
-            merge += (words[j] + " ");
-        }
-        return merge;
     }
 
 }
