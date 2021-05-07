69ebfbb8a09b6ebec8a971c0bf0ed43a3394f1be
Max Baumann <info@fosefx.com>
2021-05-07 20:13:07 +0200

    bit of a rework

--- mod/app/src/main/java/bttv/Updater.java
+++ mod/app/src/main/java/bttv/Updater.java
@@ -1,19 +1,17 @@
 package bttv;
 
 import android.app.Activity;
-import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import androidx.core.app.NotificationCompat;
+import androidx.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.concurrent.Callable;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -26,28 +24,22 @@ public class Updater {
     private static final String GH_API_HOST = "https://api.github.com";
 
     public static void checkForUpdates(final Activity activity, final PersistentBannerPresenter presenter) {
-        checkForUpdates(activity, presenter, null);
+        checkForUpdates(activity, new UIListener(activity, presenter));
     }
 
-    public static void checkForUpdates(final Context context, final PersistentBannerPresenter presenter, final UpdateCallbackListener listener) {
+    public static void checkForUpdates(@NonNull final Context context, @NonNull final UpdateCallbackListener listener) {
         Log.d("LBTTVUpdated", "Checking for updates...");
         Network.get(GH_API_HOST + "/repos/bttv-android/bttv/releases/latest", new Callback() {
 
-            private void done(boolean ok, String version) {
-                if (listener != null) {
-                    listener.onDone(ok, version);
-                }
-            }
-
             @Override
-            public void onFailure(Call call, IOException e) {
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 Log.e("LBTTVNetworkFail", "Update Call failed", e);
                 Log.e("LBTTVNetworkFail", call.toString());
-                done(false, null);
+                listener.onError(e);
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 try (ResponseBody responseBody = response.body()) {
                     if (!response.isSuccessful())
                         throw new IOException("Unexpected code " + response);
@@ -58,7 +50,7 @@ public class Updater {
                     if (tagName.equals(Data.getBttvVersion(context))) {
                         Log.d("LBTTVUpdate",
                                 "app up-to-date (version: , " + Data.getBttvVersion(context) + " gh: " + tagName + ")");
-                        done(true, null);
+                        listener.onNoUpdate();
                         return;
                     }
                     String body = json.getString("body");
@@ -70,16 +62,11 @@ public class Updater {
 
                     Log.d("LBTTVUpdater", "Update available " + Data.getBttvVersion(context) + " -> " + tagName);
 
-
-                    if (context instanceof Activity && presenter != null) {
-                        askUser((Activity) context, presenter, tagName, body, apkUrl, json.getString("html_url"));
-                    }
-
-                    done(true, tagName);
+                    listener.onUpdate(tagName, body, apkUrl, json.getString("html_url"));
 
                 } catch (JSONException e) {
                     e.printStackTrace();
-                    done(false, null);
+                    listener.onError(e);
                 }
             }
 
@@ -98,47 +85,69 @@ public class Updater {
         return null;
     }
 
-    private static void askUser(final Activity activity, final PersistentBannerPresenter presenter,
-            final String newVersion, final String body, final String apkUrl, final String htmlUrl) {
-
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                presenter.setListener(new PersistentBannerPresenterListener() {
-
-                    @Override
-                    public void installBannerShown() {
-                        Log.d("LBTTVUpdater", "Shown prompt");
-                    }
-
-                    @Override
-                    public void installUpdate() {
-                        Log.d("LBTTVUpdater", "user requested install");
-                        presenter.setShouldShowUpdateBanner(false);
-
-                        Intent intent = new Intent(activity, UpdaterActivity.class);
-                        intent.putExtra("new_version", newVersion);
-                        intent.putExtra("body", body);
-                        intent.putExtra("url", apkUrl);
-                        activity.startActivity(intent);
-
-                    }
-
-                    @Override
-                    public void updateDismissed() {
-                        Log.d("LBTTVUpdater", "user dismissed prompt");
-                    }
-
-                });
-
-                presenter.setShouldShowUpdateBanner(true);
-            }
-        });
-
+    public static Intent updateActivityIndent(@NonNull Context context, @NonNull String newVersion, @NonNull String body, @NonNull String apkUrl) {
+        Intent intent = new Intent(context, UpdaterActivity.class);
+        intent.putExtra("new_version", newVersion);
+        intent.putExtra("body", body);
+        intent.putExtra("url", apkUrl);
+        return intent;
     }
 
     public interface UpdateCallbackListener {
-        void onDone(boolean ok, String version);
+        void onNoUpdate();
+
+        void onUpdate(final String newVersion, final String body, final String apkUrl, final String htmlUrl);
+
+        void onError(final Throwable error);
+    }
+
+    private static class UIListener implements UpdateCallbackListener {
+        final Activity activity;
+        final PersistentBannerPresenter presenter;
+
+        public UIListener(final Activity activity, final PersistentBannerPresenter presenter) {
+            this.activity = activity;
+            this.presenter = presenter;
+        }
+
+        @Override
+        public void onNoUpdate() {
+        }
+
+        @Override
+        public void onUpdate(String newVersion, String body, String apkUrl, String htmlUrl) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    presenter.setListener(new PersistentBannerPresenterListener() {
+
+                        @Override
+                        public void installBannerShown() {
+                            Log.d("LBTTVUpdater", "Shown prompt");
+                        }
+
+                        @Override
+                        public void installUpdate() {
+                            Log.d("LBTTVUpdater", "user requested install");
+                            presenter.setShouldShowUpdateBanner(false);
+                            activity.startActivity(updateActivityIndent(activity, newVersion, body, apkUrl));
+                        }
+
+                        @Override
+                        public void updateDismissed() {
+                            Log.d("LBTTVUpdater", "user dismissed prompt");
+                        }
+
+                    });
+                    presenter.setShouldShowUpdateBanner(true);
+                }
+            });
+        }
+
+        @Override
+        public void onError(Throwable error) {
+            Log.e("LBTTVUpdaterUIListener", "an error has occurred while checking for updates", error);
+        }
     }
 
 }
--- mod/app/src/main/java/bttv/UpdaterJobService.java
+++ mod/app/src/main/java/bttv/UpdaterJobService.java
@@ -1,11 +1,13 @@
 package bttv;
 
+import android.app.PendingIntent;
 import android.app.job.JobInfo;
+import android.app.job.JobParameters;
 import android.app.job.JobScheduler;
 import android.app.job.JobService;
-import android.app.job.JobParameters;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 
@@ -18,19 +20,6 @@ public class UpdaterJobService extends JobService implements Updater.UpdateCallb
     final static int JOB_ID = 1873912;
     private JobParameters params = null;
 
-    @Override
-    public boolean onStartJob(JobParameters jobParameters) {
-        Log.d("LBTTVUpdaterJob", "onStartJob: " + jobParameters.toString());
-        this.params = jobParameters;
-        Updater.checkForUpdates(this, null, this);
-        return true;
-    }
-
-    @Override
-    public boolean onStopJob(JobParameters params) {
-        return true;
-    }
-
     public static void schedule(Context context) {
         if (Build.VERSION.SDK_INT < 24) {
             Log.w("LBTTVUpdaterJobSvc", "schedule: API Level is " + Build.VERSION.SDK_INT + " < 24, wont schedule job");
@@ -38,13 +27,9 @@ public class UpdaterJobService extends JobService implements Updater.UpdateCallb
         }
         JobScheduler scheduler = context.getSystemService(JobScheduler.class);
 
-        if (scheduler.getPendingJob(JOB_ID) != null) {
-            return;
-        }
-
         ComponentName serviceComponent = new ComponentName(context, UpdaterJobService.class);
         JobInfo.Builder builder = new JobInfo.Builder(UpdaterJobService.JOB_ID, serviceComponent);
-        builder.setPeriodic(24 * 60 * 60 * 1000); // 1 day in ms
+        builder.setPeriodic(JobInfo.getMinPeriodMillis()); // 24 * 60 * 60 * 1000); // 1 day in ms
 
         // battery saving features
         if (Build.VERSION.SDK_INT >= 26) {
@@ -61,28 +46,58 @@ public class UpdaterJobService extends JobService implements Updater.UpdateCallb
     }
 
     @Override
-    public void onDone(boolean ok, String version) {
-        Log.d("LBTTVUpdateJob", "onDone: " + ok);
-        if (ok && version != null) {
-            try {
-                int icon = getResources().getIdentifier("ic_twitch_glitch_uv_alpha_only", "drawable", getPackageName());
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d("LBTTVUpdaterJob", "onStartJob: " + jobParameters.toString());
+        this.params = jobParameters;
+        Updater.checkForUpdates(this, this);
+        return true;
+    }
 
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-                Notifications.createChannels(this);
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
 
-                NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(this, "bttv_android_updates")
-                        .setSmallIcon(icon)
-                        .setContentTitle("bttv-android update detected")
-                        .setContentText(Data.bttvVersion + " -> " + version)
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setAutoCancel(true);
+    @Override
+    public void onError(Throwable error) {
+        Log.d("LBTTVUpdaterJob", "onError");
+        jobFinished(params, true);
+    }
 
-                notificationManager.notify((int) (Math.random() * 10000), notifBuilder.build());
-            } catch (Throwable e) {
-                Log.e("LBTTVUpdateJub", "err creating notif ", e);
-            }
+    @Override
+    public void onNoUpdate() {
+        Log.d("LBTTVUpdaterJob", "onNoUpdate");
+        jobFinished(params, false);
+    }
+
+    @Override
+    public void onUpdate(String newVersion, String body, String apkUrl, String htmlUrl) {
+        Log.d("LBTTVUpdaterJob", "onUpdate");
+        try {
+            int icon = getResources().getIdentifier("ic_twitch_glitch_uv_alpha_only", "drawable", getPackageName());
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            Notifications.createChannels(this);
+
+            NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(this, "bttv_android_updates")
+                    .setSmallIcon(icon)
+                    .setContentTitle("bttv-android update detected")
+                    .setContentText(Data.getBttvVersion(this) + " -> " + newVersion)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(
+                            PendingIntent.getActivity(
+                                    this,
+                                    0,
+                                    Updater.updateActivityIndent(this, newVersion, body, apkUrl)
+                                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK),
+                                    0
+                            )
+                    )
+                    .setAutoCancel(true);
+
+            notificationManager.notify((int) (Math.random() * 10000), notifBuilder.build());
+        } catch (Throwable e) {
+            Log.e("LBTTVUpdaterJob", "err creating notif ", e);
         }
-        Log.d("LBTTVUpdateJob", "finished");
-        jobFinished(params, !ok);
     }
 }
