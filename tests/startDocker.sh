97dec623a40823946a656bf255d768d4a4de8f63
Max Baumann <info@fosefx.com>
2021-05-11 10:36:36 +0200

    new ci fixes

--- tests/startDocker.sh
+++ tests/startDocker.sh
@@ -6,6 +6,6 @@ docker run --rm \
 	-p 5554:5554 \
 	-p 5555:5555 \
 	-p 4723:4723 \
-	-v $(pwd)/../disass/dist:/root/tmp \
+	-v "$1":/root/tmp \
 	--name docker_android \
 	budtmo/docker-android-x86-11.0
