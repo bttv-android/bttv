f2c4c8d8cdf4661f501340683769ce3120b5449c
Max Baumann <info@fosefx.com>
2021-05-11 15:08:20 +0200

    ok mb this works

---
+++ tests/wait.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker exec docker_android adb wait-for-device
+
+A=$(docker exec docker_android adb shell getprop sys.boot_completed | tr -d '\r')
+
+while [ "$A" != "1" ]; do
+        sleep 2
+        A=$(docker exec docker_android adb shell getprop sys.boot_completed | tr -d '\r')
+done
+
+# adb shell input keyevent 82
