43e8bb7ef32a86c2af8c1a52deda7eda1dcc3302
Max Baumann <info@fosefx.com>
2021-06-15 21:09:58 +0200

    fix regression

--- mod/app/src/main/java/bttv/emote/EmoteUrlUtil.java
+++ mod/app/src/main/java/bttv/emote/EmoteUrlUtil.java
@@ -6,7 +6,12 @@ import android.util.Log;
 import tv.twitch.android.shared.emotes.utils.AnimatedEmotesUrlUtil;
 
 public class EmoteUrlUtil {
-    public static final String generateEmoteUrl(AnimatedEmotesUrlUtil util, Context context, String id, float f) {
+
+    public static String generateEmoteUrl(String id, float f) {
+        return generateEmoteUrl(null, null, id, f);
+    }
+
+    public static String generateEmoteUrl(AnimatedEmotesUrlUtil util, Context context, String id, float f) {
         if (id.startsWith("BTTV-")) {
             String realId = id.split("BTTV-")[1];
             Emote emote = Emotes.getEmoteById(realId);
@@ -15,12 +20,14 @@ public class EmoteUrlUtil {
                 return "https://cdn.betterttv.net/emote/" + realId + "/1x"; // gamble
             }
             return emote.url;
-        } else {
+        } else if (util != null) {
             return util.generateEmoteUrl(context, id, f);
+        } else {
+            return tv.twitch.android.util.EmoteUrlUtil.generateEmoteUrl(id, f);
         }
     }
 
-    public static final String getEmoteUrl(AnimatedEmotesUrlUtil util, Context c, String id) {
+    public static String getEmoteUrl(AnimatedEmotesUrlUtil util, Context c, String id) {
         if (id.startsWith("BTTV-")) {
             return EmoteUrlUtil.generateEmoteUrl(util, c, id, 1.0f);
         } else {
---
+++ mod/twitch/src/main/java/tv/twitch/android/util/EmoteUrlUtil.java
@@ -0,0 +1,7 @@
+package tv.twitch.android.util;
+
+public class EmoteUrlUtil {
+    public static String generateEmoteUrl(String emote, float f) {
+        throw new IllegalStateException("EmoteUrlUtil.generateEmoteUrl() called");
+    }
+}
