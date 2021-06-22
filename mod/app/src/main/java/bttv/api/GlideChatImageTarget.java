3236e02c9ae24d85f04abd3ad18e8c7069f0703c
Max Baumann <info@fosefx.com>
2021-06-22 15:51:49 +0200

    kind of works right now

--- mod/app/src/main/java/bttv/api/GlideChatImageTarget.java
+++ mod/app/src/main/java/bttv/api/GlideChatImageTarget.java
@@ -1,6 +1,9 @@
 package bttv.api;
 
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+import android.widget.TextView;
 
 import tv.twitch.android.shared.ui.elements.span.UrlDrawable;
 
@@ -15,5 +18,30 @@ public class GlideChatImageTarget {
         String url = urlDrawable.getUrl();
         Log.i("LBTTVDEBUG", "getIsBttvEmote: " + url);
         return url.contains("betterttv.net") || url.contains("7tv.app"); // TODO: find better way of determining this
+    }
+
+    public static void setRenderingView(tv.twitch.android.shared.ui.elements.span.GlideChatImageTarget target, TextView view) {
+        target.bttvView = view;
+    }
+
+    public static void invalidateView(tv.twitch.android.shared.ui.elements.span.GlideChatImageTarget target, Drawable drawable) {
+        try {
+            Rect bounds = drawable.getBounds();
+            if (bounds.bottom >= bounds.right) {
+                return;
+            }
+            if (!getIsBttvEmote(target)) {
+                return;
+            }
+            if (target.bttvView == null) {
+                Log.w("LBTTVGlideChatImgTarget", "invalidateView: bttvView is null");
+                return;
+            }
+            target.bttvView.invalidate();
+            target.bttvView.setText(target.bttvView.getText()); // dirty trick to force re-draw
+            target.bttvView = null; // drop ref idk how it could impact GC, and onResourceReady is only called once anyway (I hope)
+        } catch (Throwable e) {
+            Log.e("LBTTVGlideChatImgTarget", "invalidateView: error", e);
+        }
     }
 }
