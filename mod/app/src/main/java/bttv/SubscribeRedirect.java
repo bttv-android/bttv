3838153ecbba78419a0da4498609d7082fc2f344
Max Baumann <info@fosefx.com>
2021-08-28 19:09:58 +0200

    still need to convert id to channel name

--- mod/app/src/main/java/bttv/SubscribeRedirect.java
+++ mod/app/src/main/java/bttv/SubscribeRedirect.java
@@ -1,18 +1,26 @@
 package bttv;
 
+import android.util.Log;
+
 import tv.twitch.android.feature.theatre.common.PlayerCoordinatorPresenter;
 import tv.twitch.android.feature.theatre.metadata.MetadataViewEvent;
-import tv.twitch.android.models.channel.ChannelModel;
+import tv.twitch.android.models.social.ChatUser;
+import tv.twitch.android.shared.chat.chatuserdialog.ChatUserDialogPresenter;
 
 public class SubscribeRedirect {
     public static void openSubscribePage(PlayerCoordinatorPresenter presenter, MetadataViewEvent.SubscribeButtonClicked event) {
-        ChannelModel channel = event.channelModel;
+        String channel = event.channelModel.component2();
         String url = getUrl(channel);
         Util.launchBrowser(presenter.activity, url);
     }
 
-    private static String getUrl(ChannelModel channel) {
-        String name = channel.component2();
-        return "https://www.twitch.tv/subs/" + name;
+    private static String getUrl(String channel) {
+        return "https://www.twitch.tv/subs/" + channel;
+    }
+
+    public static void giftSubscription(ChatUserDialogPresenter presenter, ChatUser user) {
+        Log.i("LBTTVSubscribeRedirect", "giftSubscription: chatUser.username: " + user.username);
+        String url = getUrl(presenter.info.getChannelId() + "");
+        Util.launchBrowser(presenter.activity, url);
     }
 }
---
+++ mod/twitch/src/main/java/tv/twitch/android/shared/chat/chatuserdialog/ChatUserDialogPresenter.java
@@ -0,0 +1,6 @@
+package tv.twitch.android.shared.chat.chatuserdialog;
+
+public class ChatUserDialogPresenter {
+    public androidx.fragment.app.FragmentActivity activity;
+    public tv.twitch.android.shared.chat.chatuserdialog.ChatUserDialogInfo info;
+}
