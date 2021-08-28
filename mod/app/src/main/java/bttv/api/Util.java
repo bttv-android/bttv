4e678eee8f8900d20c0e2dd4bb1a162ddb5651c7
Max Baumann <info@fosefx.com>
2021-08-28 12:54:03 +0200

    add sub redirect

--- mod/app/src/main/java/bttv/api/Util.java
+++ mod/app/src/main/java/bttv/api/Util.java
@@ -4,4 +4,8 @@ import android.util.Log;
 
 public class Util {
     final static String TAG = "LBTTVUtil";
+
+    public static void printSubscribe() {
+        Log.i(TAG, "User attempts to subscribe");
+    }
 }
