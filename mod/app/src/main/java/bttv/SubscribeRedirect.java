aead0136b2165092143490646d3edc7b526d979e
Max Baumann <info@fosefx.com>
2021-09-09 15:18:14 +0200

    done

--- mod/app/src/main/java/bttv/SubscribeRedirect.java
+++ mod/app/src/main/java/bttv/SubscribeRedirect.java
@@ -1,6 +1,11 @@
 package bttv;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Context;
 import android.util.Log;
+import android.content.ClipboardManager;
+import android.widget.Toast;
 
 import tv.twitch.android.feature.theatre.common.PlayerCoordinatorPresenter;
 import tv.twitch.android.feature.theatre.metadata.MetadataViewEvent;
@@ -20,7 +25,17 @@ public class SubscribeRedirect {
 
     public static void giftSubscription(ChatUserDialogPresenter presenter, ChatUser user) {
         Log.i("LBTTVSubscribeRedirect", "giftSubscription: chatUser.username: " + user.username);
-        String url = getUrl(presenter.info.getChannelId() + "");
-        Util.launchBrowser(presenter.activity, url);
+
+        Activity activity = presenter.activity;
+
+        ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("username", user.username);
+        clipboard.setPrimaryClip(clip);
+
+        Toast toast = Toast.makeText(activity, ResUtil.getLocaleString(activity, Res.strings.bttv_copied_to_clipboard), Toast.LENGTH_LONG);
+        toast.show();
+
+        String url = getUrl(Data.currentBroadcasterName + "");
+        Util.launchBrowser(activity, url);
     }
 }
