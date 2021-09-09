889abee0b3065f967e2a096ec0e9622f99b0c7ad
Max Baumann <info@fosefx.com>
2021-09-09 15:22:23 +0200

    Gift subs redirect to web page (#247)

--- mod/app/src/main/java/bttv/SubscribeRedirect.java
+++ mod/app/src/main/java/bttv/SubscribeRedirect.java
@@ -1,18 +1,41 @@
 package bttv;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Context;
+import android.util.Log;
+import android.content.ClipboardManager;
+import android.widget.Toast;
+
 import tv.twitch.android.feature.theatre.common.PlayerCoordinatorPresenter;
 import tv.twitch.android.feature.theatre.metadata.MetadataViewEvent;
-import tv.twitch.android.models.channel.ChannelModel;
+import tv.twitch.android.models.social.ChatUser;
+import tv.twitch.android.shared.chat.chatuserdialog.ChatUserDialogPresenter;
 
 public class SubscribeRedirect {
     public static void openSubscribePage(PlayerCoordinatorPresenter presenter, MetadataViewEvent.SubscribeButtonClicked event) {
-        ChannelModel channel = event.channelModel;
+        String channel = event.channelModel.component2();
         String url = getUrl(channel);
         Util.launchBrowser(presenter.activity, url);
     }
 
-    private static String getUrl(ChannelModel channel) {
-        String name = channel.component2();
-        return "https://www.twitch.tv/subs/" + name;
+    private static String getUrl(String channel) {
+        return "https://www.twitch.tv/subs/" + channel;
+    }
+
+    public static void giftSubscription(ChatUserDialogPresenter presenter, ChatUser user) {
+        Log.i("LBTTVSubscribeRedirect", "giftSubscription: chatUser.username: " + user.username);
+
+        Activity activity = presenter.activity;
+
+        ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("username", user.username);
+        clipboard.setPrimaryClip(clip);
+
+        Toast toast = Toast.makeText(activity, ResUtil.getLocaleString(activity, Res.strings.bttv_copied_to_clipboard), Toast.LENGTH_LONG);
+        toast.show();
+
+        String url = getUrl(Data.currentBroadcasterName + "");
+        Util.launchBrowser(activity, url);
     }
 }
--- mod/app/src/main/java/bttv/api/SubscribeRedirect.java
+++ mod/app/src/main/java/bttv/api/SubscribeRedirect.java
@@ -1,12 +1,29 @@
 package bttv.api;
 
 import android.util.Log;
+import android.view.View;
 
 import tv.twitch.android.feature.theatre.common.PlayerCoordinatorPresenter;
 import tv.twitch.android.feature.theatre.metadata.MetadataViewEvent;
+import tv.twitch.android.models.social.ChatUser;
+import tv.twitch.android.shared.chat.chatuserdialog.ChatUserDialogPresenter;
 
 public class SubscribeRedirect {
     public static final String TAG = "LBTTVSubRedir";
+
+    public static View.OnClickListener giftSubscriptionListener(ChatUserDialogPresenter presenter, ChatUser user) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Log.d(TAG, "giftSubscriptionListener()");
+                    bttv.SubscribeRedirect.giftSubscription(presenter, user);
+                } catch (Throwable e) {
+                    Log.e(TAG, "giftSubscriptionListener: ", e);
+                }
+            }
+        };
+    }
 
     public static void subscribe(PlayerCoordinatorPresenter presenter, MetadataViewEvent.SubscribeButtonClicked event) {
         try {
@@ -16,4 +33,6 @@ public class SubscribeRedirect {
             Log.e(TAG, "subscribe: ", e);
         }
     }
+
+
 }
