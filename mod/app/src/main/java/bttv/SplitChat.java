a9772d564843e6f6475333b4e54e9c02788f465a
Max Baumann <max@bmn.dev>
2021-11-14 16:04:01 +0100

    prevent non-whitelisted ids to be affected by split chat (in theory at least)

--- mod/app/src/main/java/bttv/SplitChat.java
+++ mod/app/src/main/java/bttv/SplitChat.java
@@ -1,5 +1,7 @@
 package bttv;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,14 +34,26 @@ public class SplitChat {
             return;
         }
         View view = viewHolder.itemView;
+
+        // make sure we only change chat message items
+        boolean hasChatMessageId = view.getId() == ResUtil.getResourceId(view.getContext(), "chat_message_item", "id");
+        boolean hasChommentRootId = view.getId() == ResUtil.getResourceId(view.getContext(), "chomment_root_view", "id");
+
+        if (!hasChatMessageId && !hasChommentRootId) {
+            Log.i(TAG, "view skipped, as it's not a chat message or chomment" + viewHolder.toString());
+            reset(view);
+            return;
+        }
+
         int color = isDarkThemeEnabled()
                 ? ResUtil.getColorValue("material_grey_900")
                 : ResUtil.getColorValue("material_grey_300");
 
         boolean doChange = position % 2 == 1;
 
-        // fix VODs
-        if (view.getId() == ResUtil.getResourceId(Data.ctx, "chomment_root_view", "id")) {
+        // for some reason we can't set the background for the whole view for Chomments (VODs)
+        // so we just highlight the TextView (first child)
+        if (hasChommentRootId) {
             LinearLayout linearLayout = (LinearLayout) view;
             view = linearLayout.getChildAt(0);
         }
@@ -47,14 +61,34 @@ public class SplitChat {
         if (doChange) {
             view.setBackgroundColor(color);
         } else {
-            view.setBackground(null);
+            reset(view);
         }
 
+        //
         // fix width
+        //
+
+        // MATCH_PARENT is default for Chomments, WRAP_CONTENT for Comments
         boolean matchParent = doChange || viewHolder instanceof ChommentItemViewHolder;
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         layoutParams.width = matchParent ? ViewGroup.LayoutParams.MATCH_PARENT : ViewGroup.LayoutParams.WRAP_CONTENT;
         view.setLayoutParams(layoutParams);
+    }
+
+    private static void reset(View view) {
+        Drawable background = view.getBackground();
+        if (background == null) {
+            return;
+        }
+        if (!(background instanceof ColorDrawable)) {
+            return;
+        }
+
+        ColorDrawable colorDrawable = (ColorDrawable) background;
+        if (colorDrawable.getColor() == ResUtil.getColorValue("material_grey_900")
+            || colorDrawable.getColor() == ResUtil.getColorValue("material_grey_300")) {
+            view.setBackground(null);
+        }
     }
 
     private static boolean isDarkThemeEnabled() {
