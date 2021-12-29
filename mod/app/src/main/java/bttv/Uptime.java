cceef66edb31f6902334c7e6c2b5d503a6da01ae
Max Baumann <max@bmn.dev>
2021-12-29 14:05:38 +0100

    it works

--- mod/app/src/main/java/bttv/Uptime.java
+++ mod/app/src/main/java/bttv/Uptime.java
@@ -1,6 +1,15 @@
 package bttv;
 
+import android.os.Handler;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import tv.twitch.android.models.streams.StreamModel;
 import tv.twitch.android.models.streams.StreamType;
@@ -9,37 +18,110 @@ import tv.twitch.android.shared.player.overlay.PlayerOverlayViewDelegate;
 
 public class Uptime {
   private final static String TAG = "LBTTVUptime";
+  private final static Clock clock = new Clock();
 
-  public static void replaceLiveIndicatorWithUptime(StreamModel streamModel, PlayerOverlayViewDelegate playerOverlayViewDelegate) {
+  public static void replaceLiveIndicatorWithUptime(StreamModel streamModel, PlayerOverlayViewDelegate playerOverlayViewDelegate) throws NullPointerException {
+    if (!prepareClock(streamModel, playerOverlayViewDelegate)) {
+      clock.cancelExistingSchedule();
+      playerOverlayViewDelegate.BTTVgetBottomPlayerControlOverlayViewDelegate().updateStreamType(StreamType.LIVE_VIDEO);
+    }
+  }
+
+  public static boolean prepareClock(StreamModel streamModel, PlayerOverlayViewDelegate playerOverlayViewDelegate) {
     if (streamModel == null) {
       Log.i(TAG, "replaceLiveIndicatorWithUptime: streamModel is null");
-      return;
+      return false;
     }
 
     if (!streamModel.getStreamType().equals(StreamType.LIVE_VIDEO)) {
       Log.d(TAG, "replaceLiveIndicatorWithUptime: stream type is not LIVE_VIDEO");
-      return;
+      return false;
     }
 
     if (playerOverlayViewDelegate == null) {
       Log.i(TAG, "replaceLiveIndicatorWithUptime: playerOverlayViewDelegate is null");
-      return;
+      return false;
     }
 
     BottomPlayerControlOverlayViewDelegate bottomVD = playerOverlayViewDelegate.BTTVgetBottomPlayerControlOverlayViewDelegate();
 
     if (bottomVD == null) {
       Log.i(TAG, "replaceLiveIndicatorWithUptime: playerOverlayViewDelegate.getBottomPlayerControlOverlayViewDelegate() returned null");
-      return;
+      return false;
     }
 
     if (bottomVD.mLiveIndicatorLeftText == null) {
       Log.i(TAG, "replaceLiveIndicatorWithUptime: mLiveIndicatorLeftText is null");
-      return;
+      return false;
     }
 
-    Log.i(TAG, "replaceLiveIndicatorWithUptime: " + streamModel.createdAt);
+    Date date = parseDate(streamModel.createdAt);
 
-    bottomVD.mLiveIndicatorLeftText.setText(streamModel.createdAt);
+    if (date == null) {
+      Log.w(TAG, "replaceLiveIndicatorWithUptime: date is null (" + streamModel.createdAt + ")");
+      return false;
+    }
+
+    Log.i(TAG, "replaceLiveIndicatorWithUptime: " + streamModel.createdAt + " " + date);
+
+    clock.textViewUpdated(bottomVD.mLiveIndicatorLeftText, secondsSince(date));
+    return true;
+  }
+
+  private static Date parseDate(String dateString) {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", java.util.Locale.US);
+    simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    try {
+      return simpleDateFormat.parse(dateString);
+    } catch (ParseException e) {
+      return altParseDate(dateString);
+    }
+  }
+
+  private static Date altParseDate(String dateString) {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault());
+    simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    try {
+      return simpleDateFormat.parse(dateString);
+    } catch (ParseException e) {
+      return null;
+    }
+  }
+
+  private static long secondsSince(Date date) {
+    return (new Date().getTime() - date.getTime()) / 1000;
+  }
+
+  private static class Clock {
+    private Handler handler = new Handler();
+    private TextView textView;
+    private long secondsSinceStart;
+
+    private Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        Clock.this.textView.setText(secondsToString(secondsSinceStart++));
+        Clock.this.scheduleNextTick();
+      }
+    };
+
+    public void textViewUpdated(TextView textView, long secondsSinceStart) {
+      this.cancelExistingSchedule();
+      this.textView = textView;
+      this.secondsSinceStart = secondsSinceStart;
+      this.task.run();
+    }
+
+    private void scheduleNextTick() {
+      handler.postDelayed(task, 1000);
+    }
+
+    private void cancelExistingSchedule() {
+      handler.removeCallbacks(task);
+    }
+
+    private static String secondsToString(long secs) {
+      return DateUtils.formatElapsedTime(secs);
+    }
   }
 }
