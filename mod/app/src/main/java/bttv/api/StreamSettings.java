eececc74775b8c7328efd0261c8beea8881a5e22
Max Baumann <max@bmn.dev>
2022-01-03 19:06:43 +0100

    wip

--- mod/app/src/main/java/bttv/api/StreamSettings.java
+++ mod/app/src/main/java/bttv/api/StreamSettings.java
@@ -1,16 +1,17 @@
 package bttv.api;
 
 import android.util.Log;
-import android.view.View;
+
+import tv.twitch.android.core.mvp.viewdelegate.BaseViewDelegate;
 
 public class StreamSettings {
     private static final String TAG = "LBTTVStreamSettings";
 
-    // called in
-    public static void setupBTTVStreamSettings(View view) {
+    // called in tv.twitch.android.feature.theatre.common.StreamSettingsViewDelegate.render()
+    public static void setupBTTVStreamSettings(BaseViewDelegate viewDelegate) {
       try {
         Log.i(TAG, "setupBttvStreamSettings()");
-        bttv.settings.StreamSettings.setupBTTVStreamSettings(view);
+        bttv.settings.StreamSettings.setupBTTVStreamSettings(viewDelegate.getContentView());
       } catch (Throwable t) {
         Log.e(TAG, "setupBttvStreamSettings: ", t);
       }
--- mod/app/src/main/java/bttv/settings/StreamSettings.java
+++ mod/app/src/main/java/bttv/settings/StreamSettings.java
@@ -9,19 +9,23 @@ import bttv.ResUtil;
 import bttv.Data;
 
 public class StreamSettings {
-  public static void setupBTTVStreamSettings(View view) {
+    public static void setupBTTVStreamSettings(View view) {
     setupEnableBTTVEmotes(view);
-  }
+    }
 
-  private static void setupEnableBTTVEmotes(View view) {
-    int id = ResUtil.getResourceId(Data.ctx, Res.ids.bttv_stream_settings_enable_bttv_emotes_toggle);
-    SwitchCompat toggle = view.findViewById(id);
-    boolean checked = ResUtil.getBooleanFromSettings(Settings.BTTVEmotesEnabled);
-    toggle.setChecked(checked);
-    toggle.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-      }
-    });
-  }
+    private static void setupEnableBTTVEmotes(View view) {
+        int id = ResUtil.getResourceId(view.getContext(), Res.ids.bttv_stream_settings_enable_bttv_emotes_toggle);
+        SwitchCompat toggle = view.findViewById(id);
+        boolean checked = ResUtil.getBooleanFromSettings(Settings.BTTVEmotesEnabled);
+        toggle.setChecked(checked);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.BTTVEmotesEnabled.entry.set(
+                        v.getContext(),
+                        new UserPreferences.Entry.BoolValue(toggle.isChecked())
+                );
+            }
+        });
+    }
 }
