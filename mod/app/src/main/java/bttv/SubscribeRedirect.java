433a8d84835a37c25941ec8cfa2588bd7bfad2b9
Max Baumann <max@bmn.dev>
2022-03-24 12:57:59 +0100

    quicksave

--- mod/app/src/main/java/bttv/SubscribeRedirect.java
+++ mod/app/src/main/java/bttv/SubscribeRedirect.java
@@ -7,18 +7,10 @@ import android.util.Log;
 import android.content.ClipboardManager;
 import android.widget.Toast;
 
-import tv.twitch.android.feature.theatre.common.PlayerCoordinatorPresenter;
-import tv.twitch.android.feature.theatre.metadata.MetadataViewEvent;
 import tv.twitch.android.models.social.ChatUser;
 import tv.twitch.android.shared.chat.chatuserdialog.ChatUserDialogPresenter;
 
 public class SubscribeRedirect {
-    public static void openSubscribePage(PlayerCoordinatorPresenter presenter, MetadataViewEvent.SubscribeButtonClicked event) {
-        String channel = event.channelModel.component2();
-        String url = getUrl(channel);
-        Util.launchBrowser(presenter.activity, url);
-    }
-
     private static String getUrl(String channel) {
         return "https://www.twitch.tv/subs/" + channel;
     }
--- mod/twitch/src/main/java/tv/twitch/android/feature/theatre/metadata/MetadataViewEvent.java
+++
@@ -1,7 +0,0 @@
-package tv.twitch.android.feature.theatre.metadata;
-
-public abstract class MetadataViewEvent {
-    public static final class SubscribeButtonClicked extends MetadataViewEvent {
-        public tv.twitch.android.models.channel.ChannelModel channelModel;
-    }
-}
