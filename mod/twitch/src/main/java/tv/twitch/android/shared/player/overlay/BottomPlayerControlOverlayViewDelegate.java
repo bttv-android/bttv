19bd6cff3afb7f5d085ce52161baa36533d76643
Max Baumann <max@bmn.dev>
2022-08-09 22:56:38 +0200

    remove uptime, it's a native feature now

--- mod/twitch/src/main/java/tv/twitch/android/shared/player/overlay/BottomPlayerControlOverlayViewDelegate.java
+++
@@ -1,7 +0,0 @@
-package tv.twitch.android.shared.player.overlay;
-
-import tv.twitch.android.models.streams.StreamType;
-
-public class BottomPlayerControlOverlayViewDelegate {
-  public void updateStreamType(StreamType streamType) {}
-}
