d309d4572e1d06a651ca8cbf750c574bcae346af
Zerwin <46265592+Zerwin@users.noreply.github.com>
2024-08-16 02:44:49 +0200

    Update to 16.5.1

--- mod/app/src/main/java/bttv/api/Highlight.java
+++ mod/app/src/main/java/bttv/api/Highlight.java
@@ -1,9 +1,11 @@
 package bttv.api;
 
+import kotlin.jvm.functions.Function1;
+import android.content.Context;
 import tv.twitch.android.shared.chat.pub.ChatMessageInterface;
 
 public class Highlight {
-    public static Integer replaceNum(ChatMessageInterface chatMessageInterface, boolean z, boolean z2, int i, int i2, Object iClickableUsernameSpanListener, Object twitchUrlSpanClickListener, Object webViewSource, String str, boolean z3, Object chatFiltersSettings, Integer num) {
+    public static Integer replaceNum(ChatMessageInterface chatMessageInterface, boolean z, boolean z2, Function1<Context, Integer> function, int i, Object iClickableUsernameSpanListener, Object twitchUrlSpanClickListener, Object webViewSource, String str, boolean z3, Object chatFiltersSettings, Integer num) {
         return bttv.highlight.Highlight.replaceNum(chatMessageInterface, num);
     }
 }
